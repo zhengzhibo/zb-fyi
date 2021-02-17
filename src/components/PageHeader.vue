0714996b9b27c92e6fd9729c139fcc97cdaaa170
Alex Zheng <zhibo90@gmail.com>
2021-02-17 23:30:45 +0800

    顶部导航

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,9 +1,16 @@
 <template>
   <header class="page-header">
-    <router-link to="/">
+
+    <router-link to="/" class="brand">
       <img src="/avatar.jpg" class="avatar" />
       <span class="title">Zb.fyi</span>
     </router-link>
+
+    <div class="nav">
+      <router-link to="/" class="nav-item"><span>首页</span></router-link>
+      <router-link to="/" class="nav-item"><span>归档</span></router-link>
+      <router-link to="/" class="nav-item"><span>标签</span></router-link>
+    </div>
   </header>
 </template>
 
@@ -18,25 +25,38 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.avatar {
-  width: 44px;
-  height: 44px;
-  border-radius: 50%;
-  margin-right: 2rem;
-}
 
 .page-header {
   display: flex;
   align-items: center;
-  padding-top: 10px;
-}
+  padding-top: 1rem;
 
-.page-header a {
-  display: flex;
-  align-items: center;
+  .brand {
+
+    margin-right: 1rem;
+    display: flex;
+    align-items: center;
+
+    .avatar {
+      width: 44px;
+      height: 44px;
+      border-radius: 50%;
+      margin-right: 2rem;
+    }
+  }
+
 }
 
 .title {
   font-size: 16px;
+}
+
+.nav {
+  display: flex;
+
+  .nav-item {
+    padding: 0 1rem;
+    line-height: 44px;
+  }
 }
 </style>
