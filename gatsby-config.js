21e01322188318b01ba3653ad5d53788551e02f7
Alex Zheng <zhibo90@gmail.com>
2022-03-18 20:35:11 +0800

    改为pnpm

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-pnpm`,
     // {
     //   resolve: `gatsby-plugin-google-analytics`,
     //   options: {
